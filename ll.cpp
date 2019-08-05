a1c5d096bba16e4090984d0a39b6865201a8a8e3
Vishesh-180101089 <50824297+korovishu@users.noreply.github.com>
2019-08-05 15:09:40 +0530

    Update ll.cpp

--- ll.cpp
+++ ll.cpp
@@ -39,42 +39,49 @@ void AddFirst(int a, int b, struct node** start)
 }
 void DelFirst(struct node** start)
 {
-    node* temp = *start;
-    *start = temp->next;
-    free(temp);
+    if(*start == NULL) cout << "-1";
+    else
+    {
+        node* temp = *start;
+        *start = temp->next;
+        free(temp);
+    }    
 }
 void Del(int a, int b, struct node** start)
 {
     node* temp = *start;
 	node* prev = NULL; 
+    int i=0;
 	while(temp!=NULL){
 		if(temp->x==a && temp->y==b && prev!=NULL){
 			prev->next = temp->next;
-			free(temp);
+			free(temp); i++; break;
 		}
 		else if(prev==NULL && temp->x==a && temp->y==b){
-			DelFirst(start);
+			DelFirst(start); i++; break;
 		}
 		else{
 			prev = temp;
 			temp = temp->next;
-
 		}
 	}
+    if(i == 0) cout << "-1";
 }
 void Search(float d, struct node** start)
 {
     node* temp = *start;
+    int i=0;
     while(temp!= NULL)
     {
         int a = temp->x;
         int b = temp->y;
         if(sqrt(a*a + b*b) <= d) 
         {
-            cout << "(" << a << "," << b << ")" << " ";
+            cout << "(" << a << "," << b << ")" << " "; i++;
         }
         temp = temp->next;
     }
+    if(i==0) cout << "0";
 }
 void Search(int a, int b, struct node** start)
 {
